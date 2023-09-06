09a1d13f96b395a3429b9c113e912cbad60484aa
Shahbaz <lovescoops99@gmail.com>
2023-09-07 02:07:32 +0530

    v1 index.js port env added

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const userRouter = require('./Router/userRouter');
 const cummRouter = require('./Router/cummRouter');
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT | 5000;
 
 app.use(express.json());
 
